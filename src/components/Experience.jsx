88ac63fddb05b50d6abcdb4b0012259126e1313e
ghizalane <ghizlaneelouargui@gmail.com>
2025-04-15 16:07:59 +0100

    ajuster pdf

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  '/public/Ghizlane.elouargui-EN.pdf', //paste the link to your resume here
+                  '/Ghizlane.elouargui-EN.pdf', //paste the link to your resume here
                   '_blank'
                 )
               }
